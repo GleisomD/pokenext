522c4e3ffecb7920ccbcc285d4f050082b0ba9e9
GleisomD <gleisom.profissional@gmail.com>
2023-04-04 11:19:58 -0300

    Created API Rest Function, with getStaticProps; Created consts with api source and limit id; Created API request and call and Pokemon ID

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,32 @@
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-    return <h1>PokeNext</h1>
+export async function getStaticProps() {
+    
+    const maxPokemons = 386
+    const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+    const res = await fetch(`${api}/?limit=${maxPokemons}`) // request using fetch api, can be used Axios too. Its used limit to limited the numbers of API request
+    const data = await res.json() // Calling the API data and transforming these data in object
+
+    //Add Pokemon Index
+    data.results.forEach((item, index) => {
+        item.id = index + 1
+    })
+
+    return {
+        props: {
+            pokemons: data.results,
+        },
+    }
+}
+
+export default function Home({pokemons}) {
+    return (<div>
+            <h1>PokeNext</h1>
+            <ul>
+                {pokemons.map((pokemon) => (
+                    <li key={pokemon.id}>{pokemon.name}</li>
+                ))}
+            </ul>
+        </div>)
 }
